c369f2a4e07536b89f7ca110eb4f580f5397caf2
nageshwarp1997 <nageshwar@ctruh.com>
2025-04-05 00:37:30 +0530

    Path Navigation Added

---
+++ src/components/PathNavigation.tsx
@@ -0,0 +1,19 @@
+import { useLocation } from "react-router-dom";
+import { DropdownIcon } from "../icons";
+
+const PathNavigation = () => {
+  const { pathname } = useLocation();
+  const paths = pathname.split("/").filter((path) => path !== "");
+  return (
+    <div className="flex items-center gap-2">
+      {paths.map((path, ind) => (
+        <div key={ind} className="flex items-center gap-2">
+          <span className="capitalize line-clamp-1">{path}</span>
+          {ind < paths.length - 1 && <DropdownIcon className="-rotate-90" />}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default PathNavigation;
