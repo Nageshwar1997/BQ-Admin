f587cc03450cf6bf9e11a516e015bbfd86487c31
nageshwarp1997 <nageshwar@ctruh.com>
2025-04-04 23:30:30 +0530

    `Refactor routes and sidebar data to use PrivateRoute component`

---
+++ src/pages/products/Products.tsx
@@ -0,0 +1,7 @@
+import { Outlet } from "react-router-dom";
+
+const Products = () => {
+  return <Outlet />;
+};
+
+export default Products;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,19 +8,39 @@ import Home from "../pages/home/Home";
 import LoginRedirect from "./LoginRedirect";
 import NotFound from "../pages/error/NotFound";
 import LoadingScreen from "../components/loaders/LoadingScreen";
+import PrivateRoute from "./PrivateRoute";
+import AllProducts from "../pages/products/children/AllProducts";
+import UploadProduct from "../pages/products/children/UploadProduct";
+import Products from "../pages/products/Products";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: (
       <Suspense fallback={<LoadingScreen />}>
-        <Main />
+        <PrivateRoute>
+          <Main />
+        </PrivateRoute>
       </Suspense>
     ),
     children: [
       {
         index: true,
         element: <Home />,
+      },
+      {
+        path: "products",
+        element: <Products />,
+        children: [
+          {
+            index: true,
+            element: <AllProducts />,
+          },
+          {
+            path: "upload",
+            element: <UploadProduct />,
+          },
+        ],
       },
     ],
   },
