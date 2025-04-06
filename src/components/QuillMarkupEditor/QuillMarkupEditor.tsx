b51df1ead414bf994c8ac045da11fa24477969e0
nageshwarp1997 <nageshwar@ctruh.com>
2025-04-07 01:48:07 +0530

    Quill Editor added

---
+++ src/components/QuillMarkupEditor/QuillMarkupEditor.tsx
@@ -0,0 +1,170 @@
+import { forwardRef, RefObject, useEffect, useRef } from "react";
+import "./editor.css";
+import { InfoIcon } from "../../icons";
+import Quill, {
+  Delta,
+  // Range
+} from "quill";
+
+import {
+  addBlobUrlToImage,
+  addIdsToHeadings,
+  removeUnusedBlobUrls,
+} from "../../utils";
+
+// Import & Modify Quill Image Format to Allow Blob URLs
+const Image = Quill.import("formats/image") as {
+  sanitize?: (url: string) => string;
+};
+
+// Ensure Image has a sanitize method before modifying
+if (Image && typeof Image.sanitize === "function") {
+  Image.sanitize = function (url: string): string {
+    if (url.startsWith("blob:")) {
+      return url; // Allow Blob URLs
+    }
+    const Link = Quill.import("formats/link") as {
+      sanitize?: (url: string) => string;
+    };
+    return Link?.sanitize ? Link.sanitize(url) : url; // Use link sanitizer if available
+  };
+}
+
+// Register the modified Image format
+Quill.register("formats/image", Image, true);
+Quill.register("modules/addIdsToHeadings", addIdsToHeadings);
+
+interface EditorProps {
+  label?: string;
+  readOnly?: boolean;
+  errorText?: string;
+  className?: string;
+  onTextChange?: (delta: Delta, oldDelta: Delta, source: string) => void;
+  blobUrlsRef: RefObject<string[]>;
+  // onSelectionChange?: (
+  //   range: Range | null,
+  //   oldRange: Range | null,
+  //   source: string
+  // ) => void;
+}
+
+const QuillMarkupEditor = forwardRef<Quill | null, EditorProps>(
+  (
+    {
+      label,
+      readOnly,
+      errorText,
+      className,
+      onTextChange,
+      // onSelectionChange,
+      blobUrlsRef,
+    },
+    ref
+  ) => {
+    const containerRef = useRef<HTMLDivElement>(null);
+    const editorRef = useRef<Quill | null>(null);
+    useEffect(() => {
+      if (!containerRef.current) return;
+
+      const container = containerRef.current;
+      const editorContainer = document.createElement("div");
+      container.innerHTML = "";
+      container.appendChild(editorContainer);
+
+      const quill = new Quill(editorContainer, {
+        theme: "snow",
+        placeholder: "Write your content here...",
+        modules: {
+          toolbar: {
+            container: [
+              [{ header: [false, 4, 3, 2, 1] }],
+              ["bold", "italic", "underline"],
+              [{ list: "ordered" }, { list: "bullet" }],
+              [{ script: "sub" }, { script: "super" }],
+              [{ indent: "-1" }, { indent: "+1" }],
+              // [{ size: ["small", "medium", "large", "huge"] }],
+              ["link", "image"],
+              ["clean"],
+            ],
+            handlers: {
+              image: function () {
+                addBlobUrlToImage(quill, blobUrlsRef);
+              },
+            },
+          },
+          clipboard: {
+            matchVisual: true,
+          },
+          addIdsToHeadings: {
+            enable: true,
+          },
+        },
+      });
+
+      quill.on("text-change", (delta, oldDelta, source) => {
+        onTextChange?.(delta, oldDelta, source);
+        removeUnusedBlobUrls(quill, blobUrlsRef);
+      });
+
+      // quill.on("selection-change", (range, oldRange, source) => {
+      //   onSelectionChange?.(range, oldRange, source);
+      // });
+
+      editorRef.current = quill;
+
+      if (typeof ref === "function") {
+        ref(quill);
+      } else if (ref && "current" in ref) {
+        ref.current = quill;
+      }
+
+      return () => {
+        if (ref && "current" in ref) {
+          ref.current = null;
+        }
+
+        container.innerHTML = "";
+
+        // Revoke all blob URLs
+        blobUrlsRef.current.forEach((url) => URL.revokeObjectURL(url));
+        blobUrlsRef.current = [];
+      };
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    useEffect(() => {
+      if (editorRef.current) {
+        editorRef.current.enable(!readOnly);
+      }
+    }, [readOnly]);
+
+    return (
+      <div className={`w-full space-y-1.5 ${className}`}>
+        <div className="relative">
+          {label && (
+            <span
+              className={`text-[10px] lg:text-xs text-primary-50 absolute top-0 left-3 transform -translate-y-1/2 border border-primary-10 leading-none px-1 md:px-2 py-0.5 2xl:py-1 bg-smoke-eerie rounded`}
+            >
+              {label}
+            </span>
+          )}
+          <div
+            ref={containerRef}
+            id="custom-editor"
+            className="w-full bg-smoke-eerie rounded-lg border border-primary-10 text-primary flex flex-col gap-2"
+          />
+        </div>
+        {!readOnly && errorText && (
+          <p
+            className={`w-full text-start flex gap-1 items-center text-[11px] leading-tight mt-2 text-red-500`}
+          >
+            <InfoIcon className="w-3 h-3 md:w-4 md:h-4 fill-red-500" />
+            <span>{errorText}</span>
+          </p>
+        )}
+      </div>
+    );
+  }
+);
+
+export default QuillMarkupEditor;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,7 @@
+import { RefObject } from "react";
 import CryptoJS from "crypto-js";
-
+import Quill from "quill";
+import { v4 as uuidv4 } from "uuid";
 import { ENCRYPTION_SECRET_KEY } from "../envs/index.env";
 
 export const encryptData = (data: string): string => {
@@ -48,4 +50,72 @@ export const debounce = <Args extends unknown[]>(
     clearTimeout(timer);
     timer = setTimeout(() => fn(...args), delay);
   };
+};
+
+export const addBlobUrlToImage = (
+  quill: Quill,
+  blobUrlsRef: RefObject<string[]>
+) => {
+  const input = document.createElement("input");
+  input.setAttribute("type", "file");
+  input.setAttribute("accept", "image/*");
+  input.click();
+
+  input.onchange = async () => {
+    if (!input.files || input.files.length === 0) return;
+
+    const file = input.files[0];
+
+    // Validate file size (2MB max)
+    if (file.size > 2 * 1024 * 1024) {
+      alert("Image size must be under 2MB!");
+      return;
+    }
+
+    const blobUrl = URL.createObjectURL(file);
+
+    const range = quill.getSelection();
+    if (!range) return;
+
+    blobUrlsRef.current.push(blobUrl);
+    console.log("blobUrlsRef", blobUrlsRef.current);
+
+    quill.insertEmbed(range.index, "image", blobUrl, "user");
+
+    quill.setSelection(range.index + 1);
+  };
+};
+
+export function addIdsToHeadings(quill: Quill, options: { enable: boolean }) {
+  if (!options.enable) return;
+
+  const processHeadings = () => {
+    const headings = quill.container.querySelectorAll("h1, h2, h3, h4, h5, h6");
+    headings.forEach((heading) => {
+      if (heading.id) return;
+      heading.id = uuidv4();
+    });
+  };
+
+  const debouncedProcess = debounce(processHeadings, 200);
+  quill.on("text-change", debouncedProcess);
+}
+
+export const removeUnusedBlobUrls = (
+  quill: Quill,
+  blobUrlsRef: RefObject<string[]>
+) => {
+  const editorImages = Array.from(quill.root.querySelectorAll("img")).map(
+    (img) => img.getAttribute("src")
+  );
+
+  const removedBlobUrls = blobUrlsRef.current.filter(
+    (url) => !editorImages.includes(url)
+  );
+
+  removedBlobUrls.forEach((url) => URL.revokeObjectURL(url));
+
+  blobUrlsRef.current = blobUrlsRef.current.filter((url) =>
+    editorImages.includes(url)
+  );
 };
