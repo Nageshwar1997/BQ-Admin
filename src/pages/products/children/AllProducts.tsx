7e956b4d2af4b74eb9419ec3351f94f14d804892
nageshwarp1997 <nageshwar@ctruh.com>
2025-04-13 19:57:19 +0530

    `feat: Added dynamic grid layout and product cards to AllProducts component`

--- src/pages/products/children/AllProducts.tsx
+++ src/pages/products/children/AllProducts.tsx
@@ -20,7 +20,65 @@ const AllProducts = () => {
           }
         />
       </div>
-      <p>All Products</p>
+      <div className="w-full grid gap-4 grid-cols-[repeat(auto-fit,minmax(10rem,1fr))] base:grid-cols-[repeat(auto-fill,minmax(14rem,1fr))] xl:grid-cols-[repeat(auto-fill,minmax(16rem,1fr))]">
+        {Array.from({ length: 21 }).map((_, ind) => {
+          return (
+            <div
+              key={ind}
+              className="p-4 rounded-lg shadow-sm bg-primary-inverted space-y-4 border-rounded-corners-gradient cursor-pointer"
+            >
+              <div className="aspect-square overflow-hidden rounded-md relative">
+                <img
+                  src={
+                    ind % 2 === 0
+                      ? "https://www.sugarcosmetics.com/cdn/shop/files/Ultrastay-Transferproof-Lipstick-4_4fc4742d.jpg"
+                      : "https://cdn.shopify.com/s/files/1/0906/2558/files/658887719-artboard-1.jpg?v=1736853930"
+                  }
+                  alt="Product"
+                  className="w-full h-full object-cover aspect-auto hover:scale-105 transition-transform duration-500"
+                />
+                <span className="w-8 h-8 absolute top-1 right-1 text-[10px] flex flex-col items-center justify-center rounded-full font-semibold dark:bg-green-700 light:bg-green-600 leading-none">
+                  -23%
+                </span>
+              </div>
+              <hr className="h-px block border-none bg-gradient-line" />
+              <div className="space-y-3">
+                <p className="text-sm font-semibold line-clamp-2 text-secondary">
+                  Lorem ipsum dolor sit amet. Lorem, ipsum dolor. Lorem ipsum
+                  dolor sit.
+                </p>
+                <p className="text-xs text-tertiary line-clamp-1 border border-primary-50 w-fit px-3 py-1 rounded-full">
+                  {ind % 2 === 0
+                    ? "Lipstick Primer"
+                    : "Lip Balm Primer & Scrub"}
+                </p>
+                <div className="w-full space-y-1 text-sm font-medium text-tertiary">
+                  <p className="flex items-center gap-1">
+                    Selling Price:{" "}
+                    <span className="text-green-500">₹{999}</span>
+                  </p>
+                  <p className="flex items-center gap-1">
+                    Original Price:{" "}
+                    <span className="text-red-600 line-through">₹{999}</span>
+                  </p>
+                </div>
+                <div className="flex items-center gap-4">
+                  <Button
+                    pattern="secondary"
+                    content="Edit"
+                    className="!py-1 !rounded-[5px] !text-sm"
+                  />
+                  <Button
+                    pattern="primary"
+                    content="Delete"
+                    className="!py-1 !rounded-[5px] !text-sm"
+                  />
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
