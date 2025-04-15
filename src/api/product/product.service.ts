b5b94bbe55e606ed44474edb2f1f4b49a2bf7790
nageshwarp1997 <nageshwar@ctruh.com>
2025-04-15 18:56:31 +0530

    Removed onSettled section in product service

--- src/api/product/product.service.ts
+++ src/api/product/product.service.ts
@@ -1,10 +1,8 @@
 import { useMutation } from "@tanstack/react-query";
 import { toastErrorMessage, toastSuccessMessage } from "../../utils/toasts";
 import { upload_product } from "./product.api";
-import { useNavigate } from "react-router-dom";
 
 export const useUploadProduct = () => {
-  const navigate = useNavigate();
   return useMutation({
     mutationFn: (bodyData: FormData) => upload_product(bodyData),
     onSuccess: (data) => {
@@ -14,11 +12,6 @@ export const useUploadProduct = () => {
       toastErrorMessage(
         typeof error === "string" ? error : "Failed to upload product!"
       );
-    },
-    onSettled: (data, error) => {
-      if (data && !error) {
-        navigate("/products");
-      }
     },
   });
 };
