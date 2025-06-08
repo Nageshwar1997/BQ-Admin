9dab8598cf4446c9ee5025a55b5b5f75701ea8db
nageshwarp1997 <nageshwar@ctruh.com>
2025-06-08 19:40:00 +0530

    Added GetProductById and UpdateProduct apis

--- src/api/product/product.api.ts
+++ src/api/product/product.api.ts
@@ -52,3 +52,47 @@ export const get_all_products = async ({
     throw "Something went wrong!";
   }
 };
+
+export const get_product_by_id = async ({
+  data,
+  params,
+}: {
+  data: Record<string, string[]>;
+  params: { productId: string };
+}) => {
+  try {
+    const { method, url } = productRoutes.getProductById;
+    const response = await api.request({
+      method,
+      url: `${url}/${params.productId}`,
+      data: { populateFields: data }, // ✅ Wrap 'data' inside populateFields
+    });
+    return response.data;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      throw error?.response?.data?.message || "API Error occurred";
+    }
+    throw "Something went wrong!";
+  }
+};
+
+export const update_product = async (data: FormData) => {
+  try {
+    const admin_token = getAdminToken();
+
+    const { method, url } = productRoutes.updateProduct;
+    const response = await api.request({
+      method,
+      url,
+      data,
+      headers: { Authorization: admin_token },
+    });
+    return response.data;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      // If it's an Axios error
+      throw error?.response?.data?.message || "API Error occurred";
+    }
+    throw "Something went wrong!"; // For non-Axios errors
+  }
+};
