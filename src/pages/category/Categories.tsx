f8e717772dff03b3be050bc4d30ee58966aba083
Nageshwar <nageshpawarpatil@gmail.com>
2026-05-17 11:30:17 +0530

    feat: update package.json and package-lock.json to include @tanstack/react-table; enhance Categories component with navigation

--- src/pages/category/Categories.tsx
+++ src/pages/category/Categories.tsx
@@ -1,5 +1,24 @@
+import Navbar from '@/components/layout/navbar';
+import { ROUTES } from '@/constants/common.constants';
+import usePathParams from '@/hooks/usePathParams';
+
 const Categories = () => {
-  return <div>Categories</div>;
+  const { navigate } = usePathParams();
+  return (
+    <div className="">
+      <Navbar
+        buttons={[
+          {
+            content: 'Add Category',
+            pattern: 'primary',
+            className: 'whitespace-nowrap',
+            leftIcon: { icon: 'solar:add-circle-linear', className: '*:stroke-[2.5]' },
+            buttonProps: { onClick: () => navigate(ROUTES.CATEGORIES.ADD) },
+          },
+        ]}
+      />
+    </div>
+  );
 };
 
 export default Categories;
