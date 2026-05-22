4df7365376066240ea7527962dd4c55d6fb03777
Nageshwar <nageshpawarpatil@gmail.com>
2026-05-22 09:01:44 +0530

    feat: wrap Breadcrumb component in ScrollableGradientContainer for improved layout

--- src/components/ui/Breadcrumb.tsx
+++ src/components/ui/Breadcrumb.tsx
@@ -1,6 +1,7 @@
 import usePathParams from '@/hooks/usePathParams';
 import type { IBreadcrumb } from '@/types/component.type';
 import { Icon } from '@iconify/react';
+import ScrollableGradientContainer from '../layout/containers/ScrollableGradientContainer';
 
 const Breadcrumb = ({ className = '', customPath, customPaths }: IBreadcrumb) => {
   const { pathname, paths, navigate } = usePathParams();
@@ -17,38 +18,40 @@ const Breadcrumb = ({ className = '', customPath, customPaths }: IBreadcrumb) =>
   };
 
   return (
-    <div className={`text-secondary flex items-center gap-2 ${className}`}>
-      <div className="flex items-center gap-2">
-        <span
-          className={`line-clamp-1 capitalize ${
-            finalPathName !== '/' ? 'cursor-pointer' : 'opacity-80'
-          }`}
-          onClick={() => handleNavigate(-1)}
-        >
-          Dashboard
-        </span>
-        {activePaths.length > 0 && <Icon icon="solar:alt-arrow-right-linear" className="" />}
+    <ScrollableGradientContainer direction="horizontal" className="[&>div]:justify-start">
+      <div className={`text-secondary flex items-center gap-2 ${className}`}>
+        <div className="flex items-center gap-2">
+          <span
+            className={`line-clamp-1 capitalize ${
+              finalPathName !== '/' ? 'cursor-pointer' : 'opacity-80'
+            }`}
+            onClick={() => handleNavigate(-1)}
+          >
+            Dashboard
+          </span>
+          {activePaths.length > 0 && <Icon icon="solar:alt-arrow-right-linear" className="" />}
+        </div>
+
+        {activePaths.map((path, index) => {
+          const targetPath = '/' + activePaths.slice(0, index + 1).join('/');
+          const isLast = index === activePaths.length - 1;
+
+          return (
+            <div key={index} className="flex items-center gap-2">
+              <span
+                className={`line-clamp-1 capitalize ${
+                  !isLast && finalPathName !== targetPath ? 'cursor-pointer' : 'opacity-80'
+                }`}
+                onClick={!isLast ? () => handleNavigate(index) : undefined}
+              >
+                {path.replace('-', ' ')}
+              </span>
+              {!isLast && <Icon icon="solar:alt-arrow-down-linear" />}
+            </div>
+          );
+        })}
       </div>
-
-      {activePaths.map((path, index) => {
-        const targetPath = '/' + activePaths.slice(0, index + 1).join('/');
-        const isLast = index === activePaths.length - 1;
-
-        return (
-          <div key={index} className="flex items-center gap-2">
-            <span
-              className={`line-clamp-1 capitalize ${
-                !isLast && finalPathName !== targetPath ? 'cursor-pointer' : 'opacity-80'
-              }`}
-              onClick={!isLast ? () => handleNavigate(index) : undefined}
-            >
-              {path.replace('-', ' ')}
-            </span>
-            {!isLast && <Icon icon="solar:alt-arrow-down-linear" />}
-          </div>
-        );
-      })}
-    </div>
+    </ScrollableGradientContainer>
   );
 };
 
