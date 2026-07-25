056334ad53d4d3e9e4ed58ccc7958fac80b41a94
Nageshwar <nageshpawarpatil@gmail.com>
2026-07-26 00:11:22 +0530

    Removed unused public images

--- src/utils/crypto.util.ts
+++ src/utils/crypto.util.ts
@@ -1,5 +1,6 @@
 import { parseData, stringifyData } from '@beautinique/shared-utils';
-import CryptoJS from 'crypto-js';
+import AES from 'crypto-js/aes';
+import Utf8 from 'crypto-js/enc-utf8';
 
 import envs from '@/envs';
 
@@ -7,7 +8,7 @@ import envs from '@/envs';
 export const encryptData = <T extends object | string>(data: T): string => {
   const stringData = typeof data === 'string' ? data : stringifyData<T>(data);
 
-  const encrypted = CryptoJS.AES.encrypt(stringData, envs.encryption_secret_key);
+  const encrypted = AES.encrypt(stringData, envs.encryption_secret_key);
   return encrypted.toString();
 };
 
@@ -15,8 +16,8 @@ export const encryptData = <T extends object | string>(data: T): string => {
 export const decryptData = <T = unknown>(encryptedData: string | null): T | null => {
   if (!encryptedData) return null;
 
-  const bytes = CryptoJS.AES.decrypt(encryptedData, envs.encryption_secret_key);
-  const decrypted = bytes.toString(CryptoJS.enc.Utf8);
+  const bytes = AES.decrypt(encryptedData, envs.encryption_secret_key);
+  const decrypted = bytes.toString(Utf8);
 
   try {
     return parseData<T>(decrypted);
